d6a0a173f63a05bb6c341c3d072ba48de7429b14
kabbo500 <kabbokazi3779@gmail.com>
2022-05-08 20:22:38 +0800

    add token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -11,7 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log("decoded", decoded)
+        req.decoded = decoded;
+    })
 
+    next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r1dsd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -20,29 +36,47 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('wireHouse').collection('service')
+        const productCollection = client.db('wireHouse').collection('product')
 
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
-
+        //Product services API
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
-
+        //old product
         app.get('/service', async (req, res) => {
-            const email = req.query.email;
 
-            const query = { email: email };
+            const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
 
+        //new product
+        app.get('/product', verifyJWT, async (req, res) => {
+
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
         //post
-        app.post('/service', async (req, res) => {
+        app.post('/product', async (req, res) => {
             const newService = req.body;
-            const result = await serviceCollection.insertOne(newService);
+            const result = await productCollection.insertOne(newService);
             res.send(result);
         });
 
@@ -51,6 +85,14 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        //new product delete
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
             res.send(result);
         })
 
